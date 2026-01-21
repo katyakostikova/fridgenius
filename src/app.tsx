db1db16830078620b31c9fcf445fdbd3163ca75c
katyakostikova <katyakostikova21@gmail.com>
2026-01-21 14:34:57 +0100

    Added custom fonts

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,10 @@
+import {
+  useFonts,
+  Nunito_400Regular,
+  Nunito_500Medium,
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+} from '@expo-google-fonts/nunito';
 import { NavigationContainer } from '@react-navigation/native';
 import { getLocales } from 'expo-localization';
 
@@ -7,6 +14,17 @@ import { I18nAppText } from 'services';
 I18nAppText.locale = getLocales()[0].languageCode ?? 'en';
 
 export default function App() {
+  const [areFontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_500Medium,
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+  });
+
+  if (!areFontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <BottomTabsNavigator />
