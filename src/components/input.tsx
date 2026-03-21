47ab74403b306e65988a8ec1984528483b876b63
katyakostikova <katyakostikova21@gmail.com>
2026-03-21 17:05:33 +0100

    Changed input style

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,7 +6,7 @@ type InputProps = TextInputProps;
 const Input: FC<InputProps> = ({ className, ...props }) => {
   return (
     <TextInput
-      className={`px-3 py-4 border bg-neutral50 border-neutral300 rounded-md font-nunito text-neutral800 ${className ?? ''}`}
+      className={`px-3 py-4 border bg-neutralOn border-neutral100/60 rounded-xl font-nunito text-neutral800 ${className ?? ''}`}
       {...props}
     />
   );
